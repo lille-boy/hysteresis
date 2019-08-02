0be31a7c2c7db245edcb92a3cf003a54360d48a4
Arnaud <53575963+lille-boy@users.noreply.github.com>
2019-08-02 17:00:45 +0200

    implemented function
    missing argument check

--- hysteresis.c
+++ hysteresis.c
@@ -3,13 +3,53 @@
  * The hysteresis takes a percentage as an input
  * It ouputs a level between 0 and 4
  */
- 
-unsigned int hysteresis(unsigned int an_volt_percent) {
-    unsigned int discrete_level = 0;
-    static unsigned int prev_discrete_level;
+
+#include <stdio.h>
+
+#define THRES_LOW_POS   0
+#define THRES_HIGH_POS  1
+#define LEVEL_POS       2
+
+#define NB_THRESHOLDS   4
+
+
+enum levels {
+    LEVEL_0,
+    LEVEL_1,
+    LEVEL_2,
+    LEVEL_3,
+    LEVEL_4,
+};
+
+const unsigned int thresholds[NB_THRESHOLDS][3] = {
+    {10, 15, LEVEL_1},
+    {35, 40, LEVEL_2},
+    {60, 65, LEVEL_3},
+    {85, 90, LEVEL_4}
+};
+
+unsigned int hysteresis(unsigned int input_percent) {
     
+    unsigned int i = 0;
+    unsigned int discrete_level;
+    static unsigned int prev_input_precent = 0;
     
-    prev_discrete_level = discrete_level;
-    
+    if(input_percent >= prev_input_precent) {
+        for(i = 0; i < NB_THRESHOLDS; i++) {
+            if(input_percent >= thresholds[i][THRES_HIGH_POS]) {
+                prev_input_precent = input_percent;
+                discrete_level = thresholds[i][LEVEL_POS];
+            }
+        }
+    }
+    else {
+        for(i = 0; i < NB_THRESHOLDS; i++) {
+            if(input_percent <= thresholds[NB_THRESHOLDS-1-i][THRES_LOW_POS]) {
+                prev_input_precent = input_percent;
+                discrete_level = thresholds[NB_THRESHOLDS-1-i][LEVEL_POS] - 1;
+            }
+        }
+    }
+
     return discrete_level;
 }
